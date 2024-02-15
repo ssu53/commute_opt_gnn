a1515b6b5a29f471d80f9c8fa9722356172199dc
Shiye Su <ss3072@cl.cam.ac.uk>
2024-02-15 16:31:52 +0000

    simple training runs

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from generate_data import get_data_double_exp
 from models import GINModel
 
 
-def train_model(data, model, optimiser, loss_fn, device):
+def train_model(data, model, optimiser, loss_fn):
     model.train()
     optimiser.zero_grad()
     y_pred = model(data)
@@ -40,9 +40,9 @@ def train_eval_loop(
     lr: float,
     num_epochs: int,
     print_every,
-    device,
     loss_fn=None,
     verbose=False,
+    log_wandb=True,
 ):
     optimiser = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -51,28 +51,97 @@ def train_eval_loop(
         if verbose:
             print("Using MSE Loss...")
 
+    epoch2trainloss = {}
     epoch2valloss = {}
 
     with tqdm(range(1, num_epochs + 1), unit="e", disable=not verbose) as tepoch:
         for epoch in tepoch:
             tepoch.set_description(f"Epoch {epoch}")
             for data_train in data_loader_train:
-                train_loss = train_model(data_train, model, optimiser, loss_fn, device)
+                train_loss = train_model(data_train, model, optimiser, loss_fn)
             if epoch % print_every == 0:
                 val_loss = eval_model(data_loader_val, model, loss_fn)
                 tepoch.set_postfix(train_loss=train_loss, val_loss=val_loss)
+                epoch2trainloss[epoch] = train_loss
                 epoch2valloss[epoch] = val_loss
-                wandb.log({"train/loss": train_loss, "eval/loss": val_loss})
+                if log_wandb:
+                    wandb.log({"train/loss": train_loss, "eval/loss": val_loss})
             else:
-                wandb.log({"train/loss": train_loss})
+                if log_wandb:
+                    wandb.log({"train/loss": train_loss})
     print(
         f"Minimum validation loss was at epoch: {min(epoch2valloss, key=epoch2valloss.get)}, with loss: {min(epoch2valloss.values())}"
     )
 
+    if verbose:
+        print("Train / Validation loss by epoch")
+        print("\n".join("{!r}: {:.3f} / {:.3f}".format(epoch, epoch2trainloss[epoch], epoch2valloss[epoch]) for epoch in epoch2trainloss))
+
     return {"end": val_loss, "best": min(epoch2valloss.values())}
 
 
-def main():
+
+def quick_run():
+
+    with open("configs/debug.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    
+    rewirer = "cayley"
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    seed = 42
+    print(
+        "CONFIGS\n", 
+        f"{rewirer=}\n",
+        f"{device=}\n",
+        f"{seed=}\n",
+        config,
+    )
+
+
+    # Get data
+
+    graphs_train, graphs_val = get_data_double_exp(
+        rewirer=rewirer, 
+        device=device, 
+        c1=config["data"]["c1"], 
+        c2=config["data"]["c2"], 
+        c3=config["data"]["c3"], 
+        d=config["data"]["d"], 
+        train_size=config["data"]["train_size"],
+        val_size=config["data"]["val_size"],
+        seed=seed,
+    )
+
+    print(f"train targets: {np.mean([g.y.cpu() for g in graphs_train]):.2f} +/- {np.std([g.y.cpu() for g in graphs_train]):.3f}")
+    print(f"val targets: {np.mean([g.y.cpu() for g in graphs_val]):.2f} +/- {np.std([g.y.cpu() for g in graphs_val]):.3f}")
+
+    dl_train = DataLoader(graphs_train, batch_size=config["train"]["train_batch_size"])
+    dl_val = DataLoader(graphs_val, batch_size=config["train"]["val_batch_size"])
+
+    in_channels = graphs_train[0].x.shape[1]
+    out_channels = 1 if len(graphs_train[0].y.shape) == 0 else len(graphs_train[0].y.shape)
+
+
+    # Train
+
+    print("Training a GIN model without rewiring...")
+
+    model = GINModel(in_channels=in_channels, hidden_channels=config["model"]["hidden_channels"], num_layers=config["model"]["num_layers"], out_channels=out_channels, drop_prob=config["model"]["drop_prob"], only_original_graph=True)
+    model.to(device)
+
+    train_eval_loop(model, dl_train, dl_val, lr=config["train"]["lr"], num_epochs=config["train"]["num_epochs"], print_every=config["train"]["print_every"], verbose=True, log_wandb=False)
+
+    print("Training a GIN model with interleaved rewiring...")
+
+    model = GINModel(in_channels=in_channels, hidden_channels=config["model"]["hidden_channels"], num_layers=config["model"]["num_layers"], out_channels=out_channels, drop_prob=config["model"]["drop_prob"], interleave_diff_graph=True)
+    model.to(device)
+
+    train_eval_loop(model, dl_train, dl_val, lr=config["train"]["lr"], num_epochs=config["train"]["num_epochs"], print_every=config["train"]["print_every"], verbose=True, log_wandb=False)
+
+
+
+def run_experiment():
     with open("configs/config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
@@ -159,7 +228,7 @@ def main():
                         hidden_channels=config["model"]["hidden_channels"],
                         num_layers=config["model"]["num_layers"],
                         out_channels=out_channels,
-                        drop_prob=config["train"]["drop_prob"],
+                        drop_prob=config["model"]["drop_prob"],
                         only_original_graph=(approach == "only_original"),
                         interleave_diff_graph=(approach == "interleave"),
                         only_diff_graph=(approach == "only_diff"),
@@ -172,7 +241,6 @@ def main():
                         lr=config["train"]["lr"],
                         num_epochs=config["train"]["num_epochs"],
                         print_every=config["train"]["print_every"],
-                        device=device,
                         verbose=not config["run"]["silent"],
                     )
 
@@ -190,6 +258,13 @@ def main():
     pprint(results)
 
     # I process these to get the results table in playground.ipynb
+
+
+
+def main():
+    run_experiment()
+    # quick_run()
+
 
 
 if __name__ == "__main__":
